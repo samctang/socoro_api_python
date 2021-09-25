90332e60fe85fe63012e10145c900811a349d34f
samtang <samc.tang@gmail.com>
2021-09-25 12:44:26 -0700

    fixed route and added swagger redirect

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
-from .routers import operation
-
+from starlette.responses import RedirectResponse
+from src.routers import operation
 
 app = FastAPI()
 app.include_router(operation.router)
+
+
+@app.get("/")
+async def redirect():
+    response = RedirectResponse(url='/docs')
+    return response
