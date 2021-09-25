b2b61be9cb5745e92e3232b4c6a6764752a88fd0
samtang <samc.tang@gmail.com>
2021-09-24 17:40:03 -0700

    configured heroku postgresql

--- src/database.py
+++ src/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-connection_string = "postgresql://socoro:socoro@localhost:5432/socoro"
+connection_string = "postgresql://lpuinnfdjtcjoy:ddef5874ab7d88bad04a1ae66895a18b4bce20591075c33d8790fcaec5e56811\
+@ec2-34-233-187-36.compute-1.amazonaws.com:5432/d7n0lkjiq6p5m3"
 
 engine = create_engine(connection_string, pool_size=3, max_overflow=0)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
