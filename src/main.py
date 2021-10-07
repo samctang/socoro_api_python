1e3a94cc584e5630af19b9a9355b01ac8a76867b
samtang <samc.tang@gmail.com>
2021-10-06 23:24:16 -0700

    added cors middleware

--- src/main.py
+++ src/main.py
@@ -2,8 +2,23 @@ from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 from src.routers import operation
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 app.include_router(operation.router)
+
+origins = [
+    "http://localhost",
+    "http://localhost:3011",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
